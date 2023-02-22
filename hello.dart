9a516a12413f9c928d423fe97ed6ed837d16d5f8
Your Name <you@example.com>
2023-02-22 09:52:36 +0200

    Project added

---
+++ hello.dart
@@ -0,0 +1,6 @@
+void main() {
+  String name = "Wandile";
+  String surname = "Gumede";
+  String country = "South Africa";
+  print("My name is $name and my surname is $surname,I come from $country");
+}
